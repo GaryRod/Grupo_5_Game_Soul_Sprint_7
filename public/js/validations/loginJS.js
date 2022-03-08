712d3ba2b96271b46363dbaf43de4ddc038525fe
jhonnaa <jonnyemeni@gmail.com>
2022-03-08 19:35:15 -0300

    correcion validaciones login

--- public/js/validations/loginJS.js
+++ public/js/validations/loginJS.js
@@ -1,42 +1,49 @@
 window.addEventListener('load', function(){
+    
     let formulario = document.querySelector('#form-login');
-    alert('hola')
+    let campoEmail = document.querySelector('#email')
+    let campoContraseña = document.querySelector('#password')
+    let ulErrores= document.querySelector('div.errores-login ul');
+
+    campoEmail.focus()
     formulario.addEventListener('submit', e=>{
-        
-
-        let campoEmail = document.querySelector('#email')
-        let campoContraseña = document.querySelector('#password')
-        let ulErrores= document.querySelector('div.errores-login ul');
-
         let errores = [];
+        campoEmail.focus()
 
-        
         if(campoEmail.value ==''){
             errores.push('debes completar el campo de email')
+            campoEmail.classList.add("errorFatal");
         }
-        else if (!campoEmail.value.includes("@") && !email.value.includes(".com")) {
+        else if (!email.value.includes(".com")) {
             errores.push("Por favor, escribe un mail válido");
+            campoEmail.classList.add("errorFatal");
+            
         }
-
-
+        else{
+            campoEmail.classList.remove("errorFatal");
+            campoContraseña.focus();
+            
+        }
         
         if(campoContraseña.value ==''){
             errores.push('debes ingresar una contraseña')
+            campoContraseña.classList.add("errorFatal");
+        }
+        else{
+            campoContraseña.classList.remove("errorFatal");
         }
 
         if(errores.length > 0){
             e.preventDefault();
 
+            ulErrores.innerHTML = ''
             
-
             for (let i= 0; i < errores.length; i++) {
-                ulErrores.innerHTML += '<li>'+ errores[i] + '</li>' 
+                ulErrores.innerHTML += '<li class="text-danger">'+ errores[i] + '</li>' 
                 
             }
-            ulErrores.classList.add('alert-warning')
-            
-
         }
+        
     })
 })
 
