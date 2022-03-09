4cca6a9f90dff00013c0f6c29b0d991717d39904
Julianarieltorres998 <julianarieltorres@gmail.com>
2022-03-08 22:25:47 -0300

    Validaciones Front - Registro, cambios

--- public/js/validations/registerJS.js
+++ public/js/validations/registerJS.js
@@ -4,8 +4,6 @@ window.addEventListener("load", () => {
     let email = document.getElementById("registro-email");
     let password = document.getElementById("registro-password");
     let fechaNacimiento = document.getElementById("registro-fechaNacimiento");
-    let imagen = document.getElementById("registro-imagen");
-    let formatosAceptadosImagen = [".png", ".jpg"];
 
     formulario.addEventListener("submit", (event) => {
         let errores = [];
@@ -14,71 +12,72 @@ window.addEventListener("load", () => {
             let errorNombreVacio = "Por favor, escribe un nombre";
             errores.push(errorNombreVacio);
             nombre.classList.add("errorFatal");
+            nombre.classList.add("errorFatalLetras");
         } else if (nombre.value.length <= 2) {
             let errorNombreCorto = "Por favor, introduce un nombre con más de 2 carácteres"
             errores.push(errorNombreCorto);
             nombre.classList.add("errorFatalDos");
+            nombre.classList.add("errorFatalDosLetras");
         } else {
             nombre.classList.remove("errorFatal");
+            nombre.classList.remove("errorFatalLetras");
             nombre.classList.remove("errorFatalDos");
+            nombre.classList.remove("errorFatalDosLetras");
         }
 
         if (email.value == "") {
-            let errorEmailVacio = "Por favor, escribe un email";
+            let errorEmailVacio = "Por favor, escribe un mail";
             errores.push(errorEmailVacio);
             email.classList.add("errorFatal");
-        } else if (!email.value.includes("@") && !email.value.includes(".com")) {
+            email.classList.add("errorFatalLetras");
+        } else if (!email.value.includes(".com")) {
             let errorEmailValido = "Por favor, escribe un mail válido";
             errores.push(errorEmailValido);
             email.classList.add("errorFatalDos");
+            email.classList.add("errorFatalDosLetras");
         } else {
             email.classList.remove("errorFatal");
+            email.classList.remove("errorFatalLetras");
             email.classList.remove("errorFatalDos");
+            email.classList.remove("errorFatalDosLetras");
         }
 
         if (password.value == "") {
             let errorPasswordVacio = "Por favor, escribe una contraseña";
             errores.push(errorPasswordVacio);
             password.classList.add("errorFatal");
-        } else if (password.value.length <= 8) {
+            password.classList.add("errorFatalLetras");
+        } else if (password.value.length < 8) {
             let errorPasswordDebil = "Por favor, introduce una contraseña de al menos 8 carácteres";
             errores.push(errorPasswordDebil);
             password.classList.add("errorFatalDos");
+            password.classList.add("errorFatalDosLetras");
         } else {
             password.classList.remove("errorFatal");
+            password.classList.remove("errorFatalLetras");
             password.classList.remove("errorFatalDos");
+            password.classList.remove("errorFatalDosLetras");
         }
 
         if (fechaNacimiento.value == "") {
-            let errorFechaNacimientoVacia = "Por favor, pon una fecha de nacimiento";
+            let errorFechaNacimientoVacia = "Por favor, por una fecha de nacimiento";
             errores.push(errorFechaNacimientoVacia);
             fechaNacimiento.classList.add("errorFatal");
+            fechaNacimiento.classList.add("errorFatalLetras");
         } else {
             fechaNacimiento.classList.remove("errorFatal");
-        }
-
-        if (imagen) {
-            let nombreArchivo = imagen.value;
-            let extensionArchivo = nombreArchivo.split(".").pop();
-
-            if (!formatosAceptadosImagen.includes(extensionArchivo)) {
-                let errorFormatoInvalido = "Los únicos formatos permitidos son .png y .jpg";
-                errores.push(errorFormatoInvalido);
-                imagen.classList.add("errorFatal");
-            } else {
-                imagen.classList.remove("errorFatal");
-                true;
-            }
+            fechaNacimiento.classList.remove("errorFatalLetras");
         }
 
         if (errores.length > 0) {
             event.preventDefault();
 
             let erroresUlRegistro = document.getElementById("erroresRegistro-ul");
+            erroresUlRegistro.innerHTML = "";
 
-            errores.forEach(error => {
-                erroresUlRegistro.innerHTML += `<li class="text-danger">${error}</li>`
-            })
+            for (let i = 0; i < errores.length; i++) {
+                erroresUlRegistro.innerHTML += `<li class="text-danger">${errores[i]}</li>`
+            }
         } else {
             alert("Usuario registrado correctamente!")
         }
