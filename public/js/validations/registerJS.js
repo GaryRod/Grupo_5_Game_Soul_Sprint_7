96c35ac482bd41058583fdaefd18bcc0e04999d9
Julianarieltorres998 <julianarieltorres@gmail.com>
2022-03-15 19:41:14 -0300

    Validaciones Front - Cambios

--- public/js/validations/registerJS.js
+++ public/js/validations/registerJS.js
@@ -5,6 +5,16 @@ window.addEventListener("load", () => {
     let password = document.getElementById("registro-password");
     let fechaNacimiento = document.getElementById("registro-fechaNacimiento");
     let terminos = document.getElementById("tyc");
+    let registroImagen = document.getElementById("registro-imagen");
+    let formatoDeImagen = [".jpg", " .png"];
+    let ulRegistroErroresNombre = document.getElementById("errores-ul-registro-nombre");
+    let ulRegistroErroresEmail = document.getElementById("errores-ul-registro-email");
+    let ulRegistroErroresContra = document.getElementById("errores-ul-registro-contraseña");
+    let ulRegistroErroresFechaNacimiento = document.getElementById("errores-ul-registro-fechaNacimiento");
+    let ulRegistroErroresImagen = document.getElementById("errores-ul-registro-imagen");
+    let ulRegistroErroresTerminos = document.getElementById("errores-ul-registro-terminos");
+    let expresionRegularValidacionEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3,4})+$/;
+    let expresionRegularValidacionImagen = /\.(jpg|png|JPG|PNG)$/i;
 
     formulario.addEventListener("submit", (event) => {
         let errores = [];
@@ -31,7 +41,7 @@ window.addEventListener("load", () => {
             errores.push(errorEmailVacio);
             email.classList.add("errorFatal");
             email.classList.add("errorFatalLetras");
-        } else if (!email.value.includes(".com")) {
+        } else if (!expresionRegularValidacionEmail.test(email.value)) {
             let errorEmailValido = "Por favor, escribe un mail válido";
             errores.push(errorEmailValido);
             email.classList.add("errorFatalDos");
@@ -69,20 +79,78 @@ window.addEventListener("load", () => {
             fechaNacimiento.classList.remove("errorFatal");
             fechaNacimiento.classList.remove("errorFatalLetras");
         }
-        if(terminos.value == false){
-            let error = "Deves aceptar los terminos y condiciones"
+
+        if(!terminos.checked == true) {
+            let error = "Debes aceptar los terminos y condiciones"
             errores.push(error)
+        }
+
+        if (!(expresionRegularValidacionImagen).test(registroImagen.value)) {
+            let errorImagen = 'Los formatos permitidos son ' + formatoDeImagen;
+            errores.push(errorImagen);
         }
 
         if (errores.length > 0) {
             event.preventDefault();
 
-            let erroresUlRegistro = document.getElementById("erroresRegistro-ul");
+            ulRegistroErroresNombre.innerHTML = "";
+            ulRegistroErroresEmail.innerHTML = "";
+            ulRegistroErroresContra.innerHTML = "";
+            ulRegistroErroresFechaNacimiento.innerHTML = "";
+            ulRegistroErroresImagen.innerHTML = "";
+            ulRegistroErroresTerminos.innerHTML = "";
+
+            let errorRegistroNombreUno = errores.indexOf("Por favor, escribe un nombre");
+            let errorRegistroNombreDos = errores.indexOf("Por favor, introduce un nombre con más de 2 carácteres");
+
+            if (errorRegistroNombreUno != -1) {
+                ulRegistroErroresNombre.innerHTML += `<li class="text-danger">${errores[errorRegistroNombreUno]}</li>`
+            } else if (errorRegistroNombreDos != -1) {
+                ulRegistroErroresNombre.innerHTML += `<li class="text-danger">${errores[errorRegistroNombreDos]}</li>`
+            }
+
+            let errorRegistroEmailUno = errores.indexOf("Por favor, escribe un mail");
+            let errorRegistroEmailDos = errores.indexOf("Por favor, escribe un mail válido");
+
+            if (errorRegistroEmailUno != -1) {
+                ulRegistroErroresEmail.innerHTML += `<li class="text-danger">${errores[errorRegistroEmailUno]}</li>`
+            } else if (errorRegistroEmailDos != -1) {
+                ulRegistroErroresEmail.innerHTML += `<li class="text-danger">${errores[errorRegistroEmailDos]}</li>`
+            }
+
+            let errorRegistroContraUno = errores.indexOf("Por favor, escribe una contraseña");
+            let errorRegistroContraDos = errores.indexOf("Por favor, introduce una contraseña de al menos 8 carácteres");
+
+            if (errorRegistroContraUno != -1) {
+                ulRegistroErroresContra.innerHTML += `<li class="text-danger">${errores[errorRegistroContraUno]}</li>`
+            } else if (errorRegistroContraDos != -1) {
+                ulRegistroErroresContra.innerHTML += `<li class="text-danger">${errores[errorRegistroContraDos]}</li>`
+            }
+
+            let errorRegistroFechaNacimiento = errores.indexOf("Por favor, por una fecha de nacimiento");
+
+            if (errorRegistroFechaNacimiento != -1) {
+                ulRegistroErroresFechaNacimiento.innerHTML += `<li class="text-danger">${errores[errorRegistroFechaNacimiento]}</li>`
+            }
+
+            let errorRegistroImagen = errores.indexOf('Los formatos permitidos son '+ formatoDeImagen);
+
+            if (errorRegistroImagen != -1) {
+                ulRegistroErroresImagen.innerHTML += `<li class="text-danger">${errores[errorRegistroImagen]}</li>`
+            }
+
+            let errorRegistroTerminos = errores.indexOf("Debes aceptar los terminos y condiciones");
+
+            if (errorRegistroTerminos != -1) {
+                ulRegistroErroresTerminos.innerHTML += `<li class="text-danger">${errores[errorRegistroTerminos]}</li>`
+            }
+
+            /*let erroresUlRegistro = document.getElementById("erroresRegistro-ul");
             erroresUlRegistro.innerHTML = "";
 
             for (let i = 0; i < errores.length; i++) {
                 erroresUlRegistro.innerHTML += `<li class="text-danger">${errores[i]}</li>`
-            }
+            }*/ 
         } else {
             alert("Usuario registrado correctamente!")
         }
